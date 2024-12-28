4e895eb0b71ce1516e8fb868c602619cf4c71fcf
Ram Choudhary <ramchoudhary90860@gmail.com>
2024-12-28 15:41:18 +0530

    Add test for viewing available books

--- test_library.py
+++ test_library.py
@@ -24,3 +24,14 @@ def test_return_book():
     library.borrow_book("1234567890")
     library.return_book("1234567890")
     assert book.is_available
+
+def test_view_available_books():
+    library = Library()
+    book1 = Book("1234567890", "Test Book 1", "Test Author 1", 2023)
+    book2 = Book("0987654321", "Test Book 2", "Test Author 2", 2022)
+    library.add_book(book1)
+    library.add_book(book2)
+    library.borrow_book("1234567890")
+    available_books = library.view_available_books()
+    assert len(available_books) == 1
+    assert available_books[0] == book2
