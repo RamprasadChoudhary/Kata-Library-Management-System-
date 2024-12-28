295f61845fb377852772e45031cbb09586195922
Ram Choudhary <ramchoudhary90860@gmail.com>
2024-12-28 15:40:23 +0530

    Add test for returning books

--- test_library.py
+++ test_library.py
@@ -16,3 +16,11 @@ def test_borrow_book():
     assert not book.is_available
     with pytest.raises(BookNotAvailable):
         library.borrow_book("1234567890")
+
+def test_return_book():
+    library = Library()
+    book = Book("1234567890", "Test Book", "Test Author", 2023)
+    library.add_book(book)
+    library.borrow_book("1234567890")
+    library.return_book("1234567890")
+    assert book.is_available
