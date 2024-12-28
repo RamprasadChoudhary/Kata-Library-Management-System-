093bfa64872390904bd4713ea1358e2724af1ea6
Ram Choudhary <ramchoudhary90860@gmail.com>
2024-12-28 15:38:48 +0530

    Add test for borrowing books

--- test_library.py
+++ test_library.py
@@ -7,3 +7,12 @@ def test_add_book():
     library.add_book(book)
     assert len(library.books) == 1
     assert library.books[0] == book
+
+def test_borrow_book():
+    library = Library()
+    book = Book("1234567890", "Test Book", "Test Author", 2023)
+    library.add_book(book)
+    library.borrow_book("1234567890")
+    assert not book.is_available
+    with pytest.raises(BookNotAvailable):
+        library.borrow_book("1234567890")
